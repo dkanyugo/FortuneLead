6158a4ae99810386965eb9e4334317c3e2f514d1
dkanyugo <darrelkanyugo@gmail.com>
2026-01-22 12:20:27 +0300

    Added null checking

--- backend/register.php
+++ backend/register.php
@@ -3,15 +3,21 @@ include "./autoloader.php";
 $success = false;
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $db = Database::get_database();
-  $success = $db->add_lead($_POST['surname'], $_POST['firstname'], $_POST['email'], $_POST['phone']);
+  if ($_POST['surname'] !== NULL && $_POST['firstname'] !== NULL && $_POST['email'] !== NULL && $_POST['phone'] !== NULL){
+    $success = $db->add_lead($_POST['surname'], $_POST['firstname'], $_POST['email'], $_POST['phone']);
+  }
 }
 if ($success) {
 ?>
-<p class="noti_success">Success! We will contact you.</p>
-  <?php
+<div class="NotiSuccess" id="Notification">
+  <p>Registration successful! We will get back to you soon</p>
+</div>
+<?php
 } else {
 ?>
-<p class="noti_fail">Failed! Try again</p>
+<div class="NotiFailure" id="Notification">
+  <p>Registration Failed! Try again. If the problem persists contact us.</p>
+</div>
   <?php
 }
 ?>
